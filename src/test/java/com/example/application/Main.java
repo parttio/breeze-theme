1d9f6a078c77253ce1500176c998ee2a2570dc95
Martin Vysny <mavi@vaadin.com>
2023-05-03 08:17:03 +0300

    Fix vaadin-boot configuration

--- src/test/java/com/example/application/Main.java
+++ src/test/java/com/example/application/Main.java
@@ -1,10 +1,22 @@
 package com.example.application;
 
 import com.github.mvysny.vaadinboot.VaadinBoot;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.setProperty("vaadin.webpackForFrontendBuild", "true");
-        new VaadinBoot().withArgs(args).run();
+        new VaadinBoot() {
+            @Override
+            protected @NotNull WebAppContext createWebAppContext() throws IOException {
+                final WebAppContext context = super.createWebAppContext();
+                // see https://github.com/mvysny/vaadin-boot/issues/15 for more details
+                context.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*\\.jar|.*/classes/.*|.*/test-classes/.*");
+                return context;
+            }
+        }.withArgs(args).run();
     }
 }
